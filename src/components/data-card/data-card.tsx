16b1f1f2b5ab0f1b2dd0c1fe8d470270303ff5b4
anand <anandpromax@gmail.com>
2024-06-11 00:52:12 +0530

    refactor: Improve DataCard component by adding fallback message for no data found

--- src/components/data-card/data-card.tsx
+++ src/components/data-card/data-card.tsx
@@ -37,7 +37,7 @@ export const DataCard = ({ posts }: Props) => {
       <div className=" gap-4 flex flex-wrap justify-center">
         {filteredData?.map((post: VideoPost) => (
           <ThumbnailCard key={post.video.videoId} post={post.video} /> /* Render ThumbnailCard component for each filtered post */
-        ))}
+        )) ||<div className="text-red-500"> No data found</div>}
       </div>
     </div>
   );
